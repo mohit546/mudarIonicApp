c6ba047fdf80561986523a8c0de36bd3e9f8d1da
Mohit Patil <mohitpatil546@gmail.com>
2016-03-14 15:59:25 +0530

    myAccount and activities template added

--- mudraIonicApp/www/js/app.js
+++ mudraIonicApp/www/js/app.js
@@ -1,74 +1,74 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('mudraApp', ['ionic', 'mudraApp.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+	$ionicPlatform.ready(function() {
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	$stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+	.state('app', {
+		url: '/app',
+		abstract: true,
+		templateUrl: 'templates/menu.html',
+		controller: 'mudraAppCtrl'
+	})
 
-  .state('app.accounts', {
-    url: '/accounts',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/accounts.html'
-      }
-    }
-  })
+	.state('app.myAccount', {
+		url: '/myAccount',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/myAccount.html'
+			}
+		}
+	})
 
-  .state('app.editAccount', {
-      url: '/editAccount',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/editAccount.html'
-        }
-      }
-    })
+	.state('app.accounts', {
+		url: '/accounts',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/accounts.html'
+			}
+		}
+	})
 
-    .state('app.eventAccounting', {
-      url: '/eventAccounting',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/eventAccounting.html',
-          // controller: 'eventAccountingCtrl'
-        }
-      }
-    });
+	.state('app.editAccount', {
+		url: '/editAccount',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/editAccount.html'
+			}
+		}
+	})
 
-  // .state('app.single', {
-  //   url: '/playlists/:playlistId',
-  //   views: {
-  //     'menuContent': {
-  //       templateUrl: 'templates/playlist.html',
-  //       controller: 'PlaylistCtrl'
-  //     }
-  //   }
-  // });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/editAccount');
+	.state('app.eventAccounting', {
+		url: '/eventAccounting',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/eventAccounting.html',
+		  // controller: 'eventAccountingCtrl'
+			}
+		}
+	})
+
+	.state('app.activities', {
+		url: '/activities',
+		views: {
+			'menuContent': {
+				templateUrl: 'templates/activities.html',
+		// controller: 'PlaylistCtrl'
+			}
+		}
+	});
+
+	$urlRouterProvider.otherwise('/app/myAccount');
 });
