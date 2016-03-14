d1030be02b496bde03dd231364d0cff53f739a19
Mohit Patil <mohitpatil546@gmail.com>
2016-03-14 15:07:36 +0530

    accounts template added

--- mudraIonicApp/www/js/app.js
+++ mudraIonicApp/www/js/app.js
@@ -32,11 +32,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.accounts', {
+    url: '/accounts',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/accounts.html'
       }
     }
   })
